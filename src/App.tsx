a8a04183c11ce7bc356de21a8892ed5880367a41
Axis, the high wizard <asguilift@hotmail.com>
2023-09-25 10:14:20 +0700

    Removed Translation from i18n

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import router from "./router";
 
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import './i18n';
 import { CssBaseline } from "@mui/material";
 import ThemeProviderWrapper from "./theme/ThemeProvider";
 import { SidebarProvider } from "./contexts/SidebarContext";
