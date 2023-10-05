ec0d973239825f5aa0b34e4db4055c84cae810e5
Diego Sano <diegocsano@outlook.com>
2023-10-05 15:40:24 -0300

    feat: search filters on tasks page

--- src/content/applications/Tasks/tasks/HomeTasks/index.tsx
+++ src/content/applications/Tasks/tasks/HomeTasks/index.tsx
@@ -2,12 +2,21 @@ import { Box, Grid, Typography } from "@mui/material";
 import TuneIcon from '@mui/icons-material/Tune';
 import { useTaskService } from "src/services/tasks-service";
 import { useTaskServiceHook } from "src/content/applications/Tasks/hooks/useTaskServiceHook";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, Slider, TextField, Typography, useTheme } from "@mui/material";
+import ExpandMoreTwoToneIcon from "@mui/icons-material/ExpandMoreTwoTone";
 import CardTasks from "../CardTasks";
 import CardMultiTasks from "../CardMultiTasks";
 
 const HomeTask = () => {
+    const [title, setTitle] = useState("")
+    const [creatorRole, setCreatorRole] = useState("")
+    const [reward, setReward] = useState(0)
+    const [dueDate, setDueDate] = useState<Dayjs | null>(null)
 
+    const theme = useTheme();
     const taskService = useTaskService();
 
     // Instância do hook useTaskServiceHook
@@ -31,6 +40,33 @@ const HomeTask = () => {
             console.error(error);
         }
     }, []);
+
+    const filteredMultiTasks = multiTasksData?.filter((task) => {
+      if (title && !task.title.toLowerCase().includes(title.toLowerCase())) {
+        return false
+      }
+
+      if (creatorRole && !task.creatorRole.toLowerCase().includes(creatorRole.toLowerCase())) {
+        return false
+      }
+
+      if (reward > 0 && (reward >= Number.parseFloat(task.reward))) {
+        return false
+      }
+
+      if (dueDate && dueDate.startOf('day').isBefore(dayjs.unix(task.endDate).startOf('day'))) {
+        return false
+      }
+
+      return true
+    })
+
+    const maxReward = multiTasksData?.reduce((acc, curr) => {
+      const parsedReward = Number.parseFloat(curr.reward)
+
+      return parsedReward > acc ? parsedReward : acc
+    
+    }, 0) || 0
 
     return (
         <>
@@ -88,13 +124,104 @@ const HomeTask = () => {
                     <Box height={40} bgcolor={'#8EFFC2'} />
 
 
-                    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'max-content'} >
-                        <Box>
+                    <Grid container spacing={2} marginTop={0}>
+                        <Grid item xs={3} bgcolor={theme.palette.mode === 'dark' ? theme.colors.alpha.black[100] : theme.colors.alpha.trueWhite[100]}>
+                          <Box display="inline-flex" alignItems="center">
+                            <img
+                              src="/static/images/task/home/filtroIcon.svg"
+                              alt="Filtro"
+                            />
 
-                        </Box>
+                            <Typography fontSize={20}>
+                                Filtro
+                            </Typography>
+                          </Box>
 
-                        <CardMultiTasks multiTasksData={multiTasksData} loading={loading} />
-                    </Box >
+                          <Accordion>
+                            <AccordionSummary
+                              expandIcon={<ExpandMoreTwoToneIcon sx={{
+                                color: theme.palette.mode === 'dark' ? 'white' : 'black'
+                              }} />}
+                            >
+                              <Typography>Título</Typography>
+                            </AccordionSummary>
+
+                            <AccordionDetails>
+                              <TextField 
+                                value={title} 
+                                onChange={(e) => setTitle(e.target.value)} 
+                                label="Pesquisar por título" 
+                                variant="standard" 
+                                fullWidth 
+                              />
+                            </AccordionDetails>
+                          </Accordion>
+
+
+                          <Accordion>
+                            <AccordionSummary
+                              expandIcon={<ExpandMoreTwoToneIcon sx={{
+                                color: theme.palette.mode === 'dark' ? 'white' : 'black'
+                              }} />}
+                            >
+                              <Typography>Criador</Typography>
+                            </AccordionSummary>
+
+                            <AccordionDetails>
+                              <TextField 
+                                  value={creatorRole} 
+                                  onChange={(e) => setCreatorRole(e.target.value)} 
+                                  label="Pesquisar por criador" 
+                                  variant="standard" 
+                                  fullWidth 
+                                />
+                              </AccordionDetails>
+                          </Accordion>
+                        
+                          <Accordion>
+                            <AccordionSummary
+                              expandIcon={<ExpandMoreTwoToneIcon sx={{
+                                color: theme.palette.mode === 'dark' ? 'white' : 'black'
+                              }} />}
+                            >
+                              <Typography>Pagamento por hora</Typography>
+                            </AccordionSummary>
+
+                            <AccordionDetails>
+                              <Slider 
+                                valueLabelDisplay="auto"
+                                value={reward} 
+                                onChange={(_, value) => setReward(value as number)}
+                                min={0}
+                                max={maxReward}
+                              />
+                            </AccordionDetails>
+                          </Accordion>
+
+                          <Accordion>
+                            <AccordionSummary
+                              expandIcon={<ExpandMoreTwoToneIcon sx={{
+                                color: theme.palette.mode === 'dark' ? 'white' : 'black'
+                              }} />}
+                            >
+                              <Typography>Data de vencimento</Typography>
+                            </AccordionSummary>
+
+                            <AccordionDetails>
+                              <DatePicker
+                                label="Data de vencimento"
+                                value={dueDate}
+                                onChange={setDueDate}
+                                format="DD/MM/YYYY"
+                              />
+                            </AccordionDetails>
+                          </Accordion>
+                        </Grid>
+
+                        <Grid item xs={9}>
+                          <CardMultiTasks multiTasksData={filteredMultiTasks} loading={loading} />
+                        </Grid>
+                    </Grid>
                 </Box>
 
             </Box >
