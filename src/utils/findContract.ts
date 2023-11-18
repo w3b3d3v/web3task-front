4add2f5cdc50f2dd8b6f911fc760502148b72f50
heronlancellot <heronlancellot@id.uff.br>
2023-11-18 12:09:53 -0300

    Feat: Initial Structure LaChain

--- src/utils/findContract.ts
+++ src/utils/findContract.ts
@@ -1,9 +1,11 @@
 import { Contract31337 } from "src/contracts/31337/Contract31337";
 import { Contract137 } from "src/contracts/137/Contract137";
 import { Contract80001 } from "src/contracts/80001/Contract80001";
+import { Contract418 } from "src/contracts/418/Contract418";
 
 export let mapChainContract = new Map([
-    [31337, Contract31337],
-    [137, Contract137],
-    [80001, Contract80001]
+  [31337, Contract31337],
+  [137, Contract137],
+  [80001, Contract80001],
+  [418, Contract418],
 ]);
