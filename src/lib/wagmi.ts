054675b551d57ae2029f289d381956dea35ae12e
Diego Sano <diegocsano@outlook.com>
2023-11-22 20:37:24 -0300

    feat: wagmi config

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,63 +1,37 @@
-import { Contract, ethers } from "ethers";
-import { chain, createClient, defaultChains, useAccount } from "wagmi";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { mapChainContract } from "@/utils/findContract";
+import { configureChains, createConfig } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
-const alchemyId = process.env.ALCHEMY_API_KEY as string;
-const chains = defaultChains;
-const defaultChain = chain.mainnet;
+export enum SupportedNetworks {
+  "Polygon" = 137,
+  "Mumbai" = 80001,
+}
 
-export const client = createClient({
-	autoConnect: true,
-	connectors({ chainId }) {
-		const chain = chains.find((x) => x.id === chainId) ?? defaultChain;
-		const rpcUrl = chain.rpcUrls.alchemy
-			? `${chain.rpcUrls.alchemy}/${alchemyId}`
-			: chain.rpcUrls.default;
-		return [new MetaMaskConnector({ chains })];
-	},
+export const ENV_DEFAULT_CHAIN_ID = process.env.NEXT_PUBLIC_USE_TESTNET
+  ? SupportedNetworks.Mumbai
+  : SupportedNetworks.Polygon;
+
+const apiKey = process.env.NEXT_PUBLIC_USE_TESTNET
+  ? process.env.NEXT_PUBLIC_ALCHEMY_TESTNET_KEY
+  : process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+
+const { chains, publicClient } = configureChains(
+  [polygon, polygonMumbai],
+  [alchemyProvider({ apiKey: apiKey ?? "" }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "web3task",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "",
+  chains,
 });
 
-let signer = null;
-let provider;
-if (window.ethereum == null) {
-	// If MetaMask is not installed, we use the default provider,
-	// which is backed by a variety of third-party services (such
-	// as INFURA). They do not have private keys installed so are
-	// only have read-only access
-	console.log("MetaMask not installed; using read-only defaults");
-	provider = ethers.getDefaultProvider(1);
-} else {
-	// Connect to the MetaMask EIP-1193 object. This is a standard
-	// protocol that allows Ethers access to make all read-only
-	// requests through MetaMask.
-	//await window.ethereum.send('eth_requestAccounts');
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
-	provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-	provider.on("network", (newNetwork, oldNetwork) => {
-		// When a Provider makes its initial connection, it emits a "network"
-		// event with a null oldNetwork along with the newNetwork. So, if the
-		// oldNetwork exists, it represents a changing network
-		if (oldNetwork) {
-			window.location.reload();
-		}
-	});
-	// It also provides an opportunity to request access to write
-	// operations, which will be performed by the private key
-	// that MetaMask manages for the user.
-	signer = provider.getSigner();
-}
-
-const { chainId } = await provider.getNetwork();
-
-let contractABI_ID = mapChainContract.get(chainId);
-if (contractABI_ID == undefined) {
-	contractABI_ID = mapChainContract.get(137);
-}
-
-// Localhost
-export const tasksManagerContract = new ethers.Contract(
-	contractABI_ID.address,
-	contractABI_ID.abi,
-	signer
-);
+export { chains, wagmiConfig };
